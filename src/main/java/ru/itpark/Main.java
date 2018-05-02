421d863a04046077e29b5aa3f58beb3d34963f3f
Игорь Вячеславович <work7x7@yandex.ru>
2018-05-02 23:24:35 +0300

    sort by name, quantity, price ACS/DESC - realization

--- src/main/java/ru/itpark/Main.java
+++ src/main/java/ru/itpark/Main.java
@@ -8,14 +8,14 @@ public class Main {
 
         SellerRepository repository = new SellerRepository("jdbc:sqlite:db.sqlite3");
 
-//        repository.add(
-//                new Item(
-//                        "QWER1234",
-//                        "Notebook",
-//                        100,
-//                        25
-//                )
-//        );
+        repository.add(
+                new Item(
+                        "QWER1234",
+                        "Notebook",
+                        100,
+                        25
+                )
+        );
 
 //        System.out.println(repository.findAll());
 
@@ -41,6 +41,50 @@ public class Main {
 
         System.out.println(repository.findAll());
 
-        repository.removeByArticle("ZXCV5555");
+//        repository.removeByArticle("ZXCV5555");
+
+//        repository.add(
+//                new Item(
+//                        "POIU1111",
+//                        "Ручка гелевая черная",
+//                        15,
+//                        120
+//                )
+//        );
+//
+//        repository.add(
+//                new Item(
+//                        "MNBV2345",
+//                        "Мышь компьютерная SVEN",
+//                        499,
+//                        6
+//                )
+//        );
+//
+//        repository.add(
+//                new Item(
+//                        "ERTY9876",
+//                        "Арбуз",
+//                        150,
+//                        64
+//                )
+//        );
+//
+//        repository.add(
+//                new Item(
+//                        "LKJH4567",
+//                        "Яблоко",
+//                        250,
+//                        555
+//                )
+//        );
+
+        System.out.println(repository.sortAllByNameASC());
+        System.out.println(repository.sortAllByNameDESC());
+        System.out.println(repository.sortAllByQuantityASC());
+        System.out.println(repository.sortAllByQuantityDESC());
+        System.out.println(repository.sortAllByPriceASC());
+        System.out.println(repository.sortAllByPriceDESC());
+
     }
 }
--- src/main/java/ru/itpark/repository/SellerRepository.java
+++ src/main/java/ru/itpark/repository/SellerRepository.java
@@ -95,6 +95,175 @@ public class SellerRepository {
         return items;
     }
 
+    public List<Item> sortAllByNameASC() {
+        List<Item> items = new ArrayList<>();
+
+        try (Connection connection = DriverManager.getConnection(url)) {
+            try (Statement statement = connection.createStatement()) {
+                ResultSet resultSet = statement.executeQuery(
+                        "SELECT article, name, price, quantity FROM items ORDER BY name ASC ");
+
+                while (resultSet.next()) {
+                    items.add(
+                            new Item(
+                                    resultSet.getString("article"),
+                                    resultSet.getString("name"),
+                                    resultSet.getInt("price"),
+                                    resultSet.getInt("quantity")
+                            )
+                    );
+
+                }
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return items;
+    }
+
+    public List<Item> sortAllByNameDESC() {
+        List<Item> items = new ArrayList<>();
+
+        try (Connection connection = DriverManager.getConnection(url)) {
+            try (Statement statement = connection.createStatement()) {
+                ResultSet resultSet = statement.executeQuery(
+                        "SELECT article, name, price, quantity FROM items ORDER BY name DESC ");
+
+                while (resultSet.next()) {
+                    items.add(
+                            new Item(
+                                    resultSet.getString("article"),
+                                    resultSet.getString("name"),
+                                    resultSet.getInt("price"),
+                                    resultSet.getInt("quantity")
+                            )
+                    );
+
+                }
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return items;
+    }
+
+    public List<Item> sortAllByQuantityASC() {
+        List<Item> items = new ArrayList<>();
+
+        try (Connection connection = DriverManager.getConnection(url)) {
+            try (Statement statement = connection.createStatement()) {
+                ResultSet resultSet = statement.executeQuery(
+                        "SELECT article, name, price, quantity FROM items ORDER BY quantity ASC ");
+
+                while (resultSet.next()) {
+                    items.add(
+                            new Item(
+                                    resultSet.getString("article"),
+                                    resultSet.getString("name"),
+                                    resultSet.getInt("price"),
+                                    resultSet.getInt("quantity")
+                            )
+                    );
+
+                }
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return items;
+    }
+
+    public List<Item> sortAllByQuantityDESC() {
+        List<Item> items = new ArrayList<>();
+
+        try (Connection connection = DriverManager.getConnection(url)) {
+            try (Statement statement = connection.createStatement()) {
+                ResultSet resultSet = statement.executeQuery(
+                        "SELECT article, name, price, quantity FROM items ORDER BY quantity DESC ");
+
+                while (resultSet.next()) {
+                    items.add(
+                            new Item(
+                                    resultSet.getString("article"),
+                                    resultSet.getString("name"),
+                                    resultSet.getInt("price"),
+                                    resultSet.getInt("quantity")
+                            )
+                    );
+
+                }
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return items;
+    }
+
+    public List<Item> sortAllByPriceASC() {
+        List<Item> items = new ArrayList<>();
+
+        try (Connection connection = DriverManager.getConnection(url)) {
+            try (Statement statement = connection.createStatement()) {
+                ResultSet resultSet = statement.executeQuery(
+                        "SELECT article, name, price, quantity FROM items ORDER BY price ASC ");
+
+                while (resultSet.next()) {
+                    items.add(
+                            new Item(
+                                    resultSet.getString("article"),
+                                    resultSet.getString("name"),
+                                    resultSet.getInt("price"),
+                                    resultSet.getInt("quantity")
+                            )
+                    );
+
+                }
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return items;
+    }
+
+    public List<Item> sortAllByPriceDESC() {
+        List<Item> items = new ArrayList<>();
+
+        try (Connection connection = DriverManager.getConnection(url)) {
+            try (Statement statement = connection.createStatement()) {
+                ResultSet resultSet = statement.executeQuery(
+                        "SELECT article, name, price, quantity FROM items ORDER BY price DESC ");
+
+                while (resultSet.next()) {
+                    items.add(
+                            new Item(
+                                    resultSet.getString("article"),
+                                    resultSet.getString("name"),
+                                    resultSet.getInt("price"),
+                                    resultSet.getInt("quantity")
+                            )
+                    );
+
+                }
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return items;
+    }
+
+
     private void init() {
         try (Connection connection = DriverManager.getConnection(url)) {
             try (Statement statement = connection.createStatement()) {
